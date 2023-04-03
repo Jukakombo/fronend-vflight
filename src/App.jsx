47d04902d4d4efc43e8ff5787fcbef8ff57658c4
github username <email@gmail.com>
2023-04-03 02:33:48 +0000

    xflight  tonight

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import BookingDetail from "./components/BookingDetail";
+import SearchAirline from "./components/SearchAirline";
+
+import Home from "./Home";
+import Membership from "./payment/Membership";
+import Register from "./payment/Register";
+import PaymentScreen from "./payment/PaymentScreen";
+import Reservation from "./components/Reservation";
+import { getRegistrations } from "./actions/registration";
+import { useDispatch } from "react-redux";
+import Database from "./components/Database";
+import { useState } from "react";
+import xloader from "./assets/x.svg";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRegistrations());
+  }, [dispatch]);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div className="bg-[rgb(241, 242, 243)]  items-center  flex justify-center loaderx">
+          <div className="flex items-center">
+            <img className="w-[70px]" src={xloader} alt="Loading" />
+            <h1 className="font-bold">Loading...</h1>
+          </div>
+        </div>
+      ) : (
+        <div className="main">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/vflight" exact element={<SearchAirline />} />
+            <Route path="/receipt" exact element={<BookingDetail />} />
+            <Route path="/create-account" exact element={<Membership />} />
+            <Route path="/login" exact element={<Membership />} />
+            <Route path="/seat-selection" exact element={<Register />} />
+            <Route path="/payment" exact element={<PaymentScreen />} />
+            <Route path="/database" exact element={<Database />} />
+
+            <Route path="/reservation/:id" exact element={<Reservation />} />
+          </Routes>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/data.jsx
@@ -0,0 +1,78 @@
+import casablanca from "../assets/hassan-ii-mosque-surrounded-by-water-buildings-blue-sky-sunlight.jpg";
+import london from "../assets/famous-tower-bridge-evening-london-england.jpg";
+import paris from "../assets/famous-eiffel-tower-paris-with-gorgeous-colors.jpg";
+import cairo from "../assets/viewpoint-alabaster-mosque-blue-sky-cairo-egypt.jpg";
+import canada from "../assets/toronto-skyline-from-park.jpg";
+import pretoria from "../assets/boston-city-aerial-view-with-urban-buildings-highway.jpg";
+
+import x1 from "../assets/x (1).jpg";
+import x2 from "../assets/x (2).jpg";
+import x3 from "../assets/x (3).jpg";
+export const city_destination = [
+  {
+    city: "Casablanca",
+    country: "Morocco",
+    id: "kjfdk12",
+    image: casablanca,
+    price: 120,
+    date: "27 March 2023",
+  },
+  {
+    city: "Paris",
+    country: "France",
+    id: "kjfddjkks2",
+    image: paris,
+    price: 220,
+    date: "30 March 2023",
+  },
+  {
+    city: "London",
+    country: "United Kinkdom",
+    id: "kjfddjjdkkks2",
+    image: london,
+    price: 500,
+    date: "2 April 2023",
+  },
+  {
+    city: "Cairo",
+    country: "Egypt",
+    id: "kjfddjoldkkks2",
+    image: cairo,
+    price: 900,
+    date: "29 March 2023",
+  },
+  {
+    city: "Pretoria",
+    country: "South Africa",
+    id: "jhdd5445",
+    image: pretoria,
+    price: 900,
+    date: "7 April 2023",
+  },
+  {
+    city: "Ottowa",
+    country: "Canada",
+    id: "jhdd55",
+    image: canada,
+    price: 100,
+    date: "29 June 2023",
+  },
+];
+
+export const sliders = [
+  {
+    id: "uiojdvc344",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa earum deleniti dicta cum sed explicabo delectus nam sequi recusandae similique",
+    image: x1,
+  },
+  {
+    id: "uiojdvc344",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa earum deleniti dicta cum sed explicabo delectus nam sequi recusandae similique",
+    image: x2,
+  },
+  {
+    id: "uiojdvc344",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa earum deleniti dicta cum sed explicabo delectus nam sequi recusandae similique",
+    image: x3,
+  },
+];
